ece9691e1077ae6ba8ddb636bdfdd1f18afe7e22
ericauv <eriauv17@gmail.com>
2019-09-29 17:50:40 -0400

    add frontend

---
+++ frontend/src/components/YoutubeForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+const YoutubeForm = () => {
+  return <div></div>;
+};
+
+export default YoutubeForm;
